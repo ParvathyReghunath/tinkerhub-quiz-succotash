60829fe664d966f965e2703a73ab2f984d0d5a4c
Aiswarya-AM <aiswarya86065@gmail.com>
2021-08-18 14:30:10 +0530

    type welcome to quiz to press Q to exit

--- quiz.c
+++ quiz.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
-void main(){
-    printf("quiz");
-    printf("mangu");
-    printf("arjun");
-    printf("rakhing");
+void main()
+{
+ printf("\n\t\t________________________________________");
+
+     printf("\n\t\t\t        WELCOME ");
+     printf("\n\t\t\t          TO ");
+     printf("\n\t\t\t       QUIZ GAME ");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t    TEST YOUR GK..SET YOUR HIGHSCORES!!    ") ;
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t -> Press S to start the game");
+     printf("\n\t\t -> Press V to view the highest score  ");
+     printf("\n\t\t -> Press R to reset score");
+     printf("\n\t\t -> Press H for help            ");
+     printf("\n\t\t -> Press Q to quit             ");
+     printf("\n\t\t________________________________________\n\n");
 }
